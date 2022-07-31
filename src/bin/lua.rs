2178d253a4de0234acad28b321f967bff44d8ea7
Alex Orlenko <zxteam@protonmail.com>
2022-07-31 12:13:40 +0100

    Initial commit

---
+++ src/bin/lua.rs
@@ -0,0 +1,21 @@
+extern "C" {
+    pub fn lua_main(argc: libc::c_int, argv: *mut *mut libc::c_char) -> libc::c_int;
+}
+
+fn main() {
+    let mut args: Vec<*mut libc::c_char> = Vec::new();
+    for arg in ::std::env::args() {
+        args.push(
+            (::std::ffi::CString::new(arg))
+                .expect("Failed to convert argument into CString.")
+                .into_raw(),
+        );
+    }
+    args.push(::core::ptr::null_mut());
+    unsafe {
+        ::std::process::exit(lua_main(
+            (args.len() - 1) as libc::c_int,
+            args.as_mut_ptr() as *mut *mut libc::c_char,
+        ) as i32)
+    }
+}
